b1c0890e99741871a60142715f5bb5c546c6d3d8
Michelle Rodríguez <promichfsd@gmail.com>
2025-10-06 20:54:10 -0300

    MMA2

--- backend/src/controllers/ccm.controller.js
+++ backend/src/controllers/ccm.controller.js
@@ -619,7 +619,7 @@ export const descargarConformidadPDF = async (req, res) => {
       </tr>
       <tr>
         <td>${escapeHTML(vars.certificadorNombre)}</td>
-        <td>${escapeHTML(vars.certificadorLicString)}</td>
+        <td>MMA - ${escapeHTML(vars.certificadorLicString)}</td>
         <td></td>
       </tr>
     </table>
