233a23ea1d0e7dc7058cc584c60ec7dffa9de16b
Michelle Rodríguez <promichfsd@gmail.com>
2025-10-07 08:25:21 -0300

    ok

--- backend/src/routes/plantillas.routes.js
+++ backend/src/routes/plantillas.routes.js
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import { descargarPlantillaEnBlanco } from '../controllers/ot.controller.js';
+import { descargarPlantillaEnBlanco } from '../controllers/ordenTrabajo.controller.js';
 
 const router = Router();
 router.get('/:tipo', descargarPlantillaEnBlanco);
